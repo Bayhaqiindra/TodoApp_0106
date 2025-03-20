e1bede581ca2c726fa8ab4e7cf28fce818d1d06a
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-03-21 00:10:11 +0700

    Ubah tampilan SnackBar jadi transparan & melayang

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -32,41 +32,45 @@ class _TaskManagerScreenState extends State<TaskManagerScreen> {
   String? _dateError;
 
   void _addTask() {
-  setState(() {
-    _taskError = _taskController.text.isEmpty ? "Task tidak boleh kosong!" : null;
-    _dateError = _selectedDate == null ? "Tanggal harus dipilih!" : null;
+    setState(() {
+      _taskError = _taskController.text.isEmpty ? "Task tidak boleh kosong!" : null;
+      _dateError = _selectedDate == null ? "Tanggal harus dipilih!" : null;
 
-    if (_taskError == null && _dateError == null) {
-      _tasks.add({
-        'task': _taskController.text,
-        'date': _selectedDate!,
-        'done': false,
-      });
-      _taskController.clear();
-      _selectedDate = null;
+      if (_taskError == null && _dateError == null) {
+        _tasks.add({
+          'task': _taskController.text,
+          'date': _selectedDate!,
+          'done': false,
+        });
+        _taskController.clear();
+        _selectedDate = null;
 
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Container(
-            width: double.infinity, 
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16), 
-            decoration: BoxDecoration(
-              color: Colors.teal,
-              borderRadius: BorderRadius.circular(8),
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text(
+                "Task added successfully",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
             ),
-            child: const Text(
-              "Task added successfully",
-              textAlign: TextAlign.left, 
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-                fontWeight: FontWeight.w400, 
+            backgroundColor: Colors.transparent, 
+            behavior: SnackBarBehavior.floating,
+            margin: const EdgeInsets.symmetric(horizontal: 1, vertical: 5), 
+            duration: const Duration(seconds: 10),
           ),
-        ),
-          ),
-        ),
-      );
-    }
-  });
-}
+        );
+      }
+    });
+  }
 }
